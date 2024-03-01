d23c57e77c318f408860f032e4fb91d22bd9c37e
JoyRume <joy.rume@student.moringaschool.com>
2024-03-01 16:18:18 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
-
+import ipdb
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 0
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    numbers = [num ** 2 for num in int_list]
+    return numbers
 
-def fizzbuzz():
+def fizzbuzz(i):
     # code goes here!
-    pass
+    if i % 3 == 0 and i % 5 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return i 
